89e4e4e54f27c090eae6c5d913c48ced31b1cf6a
DryreL <gokdenizcetintr@gmail.com>
2024-09-14 23:28:28 +0300

    UE 5.4 Upgrade

--- Source/COAEditor.Target.cs
+++ Source/COAEditor.Target.cs
@@ -8,7 +8,12 @@ public class COAEditorTarget : TargetRules
 	public COAEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		CppStandard = CppStandardVersion.Default;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+		bLegacyParentIncludePaths = false;
+		WindowsPlatform.bStrictConformanceMode = true;
+		bOverrideBuildEnvironment = true;
 
 		ExtraModuleNames.AddRange( new string[] { "COA" } );
 	}
